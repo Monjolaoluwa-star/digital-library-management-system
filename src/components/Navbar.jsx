b985d53c2ec1a826d0f1022ddd56f75a22e9c9b7
Raphael Udeji <raphaeludeji7@gmail.com>
2026-08-11 09:04:52 +0100

    Update Navbar and add react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import logo from "../assets/ReadSpherelogo.png";
 import { Link } from "react-router-dom";
+
 import {
   FiHeart,
   FiBell,
@@ -12,9 +13,12 @@ function Navbar() {
   return (
     <header className="navbar">
 
-      {/* TOP BAR */}
+      {/* =========================
+          TOP NAVBAR
+      ========================== */}
       <div className="top-navbar">
 
+        {/* ReadSphere Logo */}
         <div className="logo">
           <img
             src={logo}
@@ -23,30 +27,48 @@ function Navbar() {
           />
         </div>
 
+
+        {/* Search Bar */}
         <div className="search-bar">
+
+          {/* Search icon */}
           <FiSearch className="search-icon" />
 
+          {/* Search input */}
           <input
             type="text"
             placeholder="Search books, authors..."
           />
+
         </div>
 
+
+        {/* Right-side icons */}
         <div className="nav-icons">
 
+          {/* Favourite books */}
           <FiHeart />
+
+          {/* Notifications */}
           <FiBell />
+
+          {/* Borrowed books / bag */}
           <FiShoppingBag />
 
-          {/* Profile Image */}
+
+          {/* Profile image */}
           <img
             src="https://i.pravatar.cc/100"
             alt="Profile"
             className="profile"
           />
 
-          {/* Login Button */}
-          <Link to="/login" className="login-link">
+
+          {/* Login */}
+          <Link
+            to="/login"
+            className="login-link"
+          >
             Login
           </Link>
 
@@ -55,25 +77,42 @@ function Navbar() {
       </div>
 
 
-      {/* BOTTOM BAR */}
+      {/* =========================
+          BOTTOM NAVBAR
+      ========================== */}
       <div className="bottom-navbar">
 
         <ul className="nav-links">
 
+          {/* Home */}
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              Home
+            </Link>
           </li>
 
+
+          {/* Browse Books */}
           <li>
-            <Link to="/browse">Browse</Link>
+            <Link to="/browse">
+              Browse
+            </Link>
           </li>
 
+
+          {/* Categories */}
           <li>
-            <Link to="/categories">Categories</Link>
+            <Link to="/categories">
+              Categories
+            </Link>
           </li>
 
+
+          {/* Borrowed Books */}
           <li>
-            <Link to="/borrowed">Borrowed</Link>
+            <Link to="/borrowed">
+              Borrowed
+            </Link>
           </li>
 
         </ul>
