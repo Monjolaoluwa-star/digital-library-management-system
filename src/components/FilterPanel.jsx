b5a80abfcc2bd9095e9d1916dba05bcc3893a714
Monjolaoluwa-star <ladipomonjola@gmail.com>
2026-08-18 10:39:38 +0100

    Fix all genres filter

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -2,15 +2,35 @@ function FilterPanel({ selectedGenre, setSelectedGenre }) {
   return (
     <select
       value={selectedGenre}
-      onChange={(event) => setSelectedGenre(event.target.value)}
+      onChange={(event) =>
+        setSelectedGenre(event.target.value)
+      }
     >
-      <option value="All">All Genres</option>
-      <option value="Programming">Programming</option>
-      <option value="Science">Science</option>
-      <option value="History">History</option>
-      <option value="Business">Business</option>
-      <option value="Self Help">Self Help</option>
-      <option value="Productivity">Productivity</option>
+      <option value="">All Genres</option>
+
+      <option value="Programming">
+        Programming
+      </option>
+
+      <option value="Science">
+        Science
+      </option>
+
+      <option value="History">
+        History
+      </option>
+
+      <option value="Business">
+        Business
+      </option>
+
+      <option value="Self Help">
+        Self Help
+      </option>
+
+      <option value="Productivity">
+        Productivity
+      </option>
     </select>
   );
 }
